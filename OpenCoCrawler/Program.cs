3da6953b0ddd81f3e2472aa3359dd2951e611acf
Ans bilal <ans.bilal@sixlogics.com>
2020-01-17 18:04:33 +0500

    sorted by sale date

---
+++ OpenCoCrawler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCoCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExcelManager.testRead();
+            ExcelManager.SortSalesRecords();
+        }
+    }
+}
